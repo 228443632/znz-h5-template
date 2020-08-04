b3a990346a9febb92de164272959179eeedaa631
逃亡猪i <bianpengfei@aliyun.com>
2020-08-04 11:31:43 +0800

    feat：修改api，添加常用的接口

--- src/api/index.js
+++ src/api/index.js
@@ -6,12 +6,12 @@ const instance = axios.create({
   timeout: 5000 // 请求超时时间
 })
 
-instance.defaults.headers.post['Content-Type'] =
-  'application/x-www-form-urlencoded;charset=UTF-8' // 'application/json';
+instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8' // 'application/json';
 instance.defaults.baseURL = 'https://api.compassedu.hk/index.php/api/'
-//const base_api = 'https://api.compassedu.hk/index.php/api/'
-const base_api = 'http://118.24.185.42:880/api/'
+// const base_api = 'https://api.compassedu.hk/index.php/api/'
+// const base_api = 'http://118.24.185.42:880/api/'
 const base_api_883 = 'http://118.24.185.42:883/api/'
+const base_api = 'http://118.24.185.42:880/api/'
 
 // const base_api_v1 = 'https://api.compassedu.hk/index.php/api/v1/'
 
@@ -51,145 +51,83 @@ instance.interceptors.response.use(
   }
 )
 
+const post = async ({ url, params = {}, rnInfo }) => {
+  var headers = {}
+  if (rnInfo) {
+    headers = {
+      Authorization: rnInfo.token,
+      uid: rnInfo.uid,
+      devicetype: rnInfo.deviceType,
+      token: rnInfo.token,
+      deviceid: rnInfo.deviceId,
+      version: rnInfo.version,
+      mobiletype: rnInfo.mobiletype
+    }
+  }
+  return new Promise((resolve, reject) => {
+    instance
+      .post(
+        url,
+        {
+          sign: 'last',
+          timestamp: Number(new Date()),
+          ...params
+        },
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            appkey: 'compassedu',
+            ...headers
+          }
+        }
+      )
+      .then(res => {
+        resolve(res)
+      })
+      .catch(error => {
+        reject(error)
+      })
+  })
+}
+
 export default {
-  getArticleDetail(id, rnInfo) {
-    var headers = {}
-    if (rnInfo) {
-      headers = {
-        Authorization: rnInfo.token,
-        uid: rnInfo.uid,
-        devicetype: rnInfo.deviceType,
-        token: rnInfo.token,
-        deviceid: rnInfo.deviceId,
-        version: rnInfo.version,
-        mobiletype: rnInfo.mobiletype
-      }
-    }
-    return instance.post(
-      base_api + 'article/view',
-      {
-        sign: 'last',
-        timestamp: Number(new Date()),
-        id: id
-      },
-      {
-        headers: {
-          appkey: 'compassedu',
-          ...headers
-        }
-      }
-    )
+  /**
+   * 目录 class level => 1:返回全部层级  0:一层
+   * */
+  categoryClass({ parent_id = '', level = '' }, rnInfo) {
+    if (parent_id === '' || level === '') throw new Error('缺少parent_id或level参数！')
+    return post({ url: base_api + 'category/class', params: data, rnInfo: rnInfo })
   },
-  getTagArticles(id, rnInfo) {
-    var headers = {}
-    if (rnInfo) {
-      headers = {
-        Authorization: rnInfo.token,
-        uid: rnInfo.uid,
-        devicetype: rnInfo.deviceType,
-        token: rnInfo.token,
-        deviceid: rnInfo.deviceId,
-        version: rnInfo.version,
-        mobiletype: rnInfo.mobiletype
-      }
-    }
-    return instance.post(
-      base_api + 'article/list/' + id,
-      {
-        sign: 'last',
-        timestamp: Number(new Date())
-      },
-      {
-        headers: {
-          appkey: 'compassedu',
-          ...headers
-        }
-      }
-    )
+
+  /**
+   * 目录 list level => 1:返回全部层级  0:一层
+   * */
+  categoryList({ parent_id = '', level = '' }, rnInfo) {
+    if (parent_id === '' || level === '') throw new Error('缺少parent_id或level参数！')
+    return post({ url: base_api + 'category/list', params: data, rnInfo: rnInfo })
   },
-  getCollectArticles(rnInfo) {
-    var headers = {}
-    if (rnInfo) {
-      headers = {
-        Authorization: rnInfo.token,
-        uid: rnInfo.uid,
-        devicetype: rnInfo.deviceType,
-        token: rnInfo.token,
-        deviceid: rnInfo.deviceId,
-        version: rnInfo.version,
-        mobiletype: rnInfo.mobiletype
-      }
-    }
-    return instance.post(
-      base_api + 'article/collect',
-      {
-        sign: 'last',
-        timestamp: Number(new Date())
-      },
-      {
-        headers: {
-          appkey: 'compassedu',
-          ...headers
-        }
-      }
-    )
+
+  /**
+   * 获取国家 类目
+   * level => 1:返回全部层级  0:一层
+   * */
+  categoryCtry(data = {}, rnInfo) {
+    return post({ url: base_api + 'category/list', params: { parent_id: 3, level: 1, ...data }, rnInfo: rnInfo })
   },
-  collectArticle(id, rnInfo) {
-    var headers = {}
-    if (rnInfo) {
-      headers = {
-        Authorization: rnInfo.token,
-        uid: rnInfo.uid,
-        devicetype: rnInfo.deviceType,
-        token: rnInfo.token,
-        deviceid: rnInfo.deviceId,
-        version: rnInfo.version,
-        mobiletype: rnInfo.mobiletype
-      }
-    }
-    return instance.post(
-      base_api_883 + 'collection/add',
-      {
-        sign: 'last',
-        timestamp: Number(new Date()),
-        type: 7,
-        cid: id
-      },
-      {
-        headers: {
-          appkey: 'compassedu',
-          ...headers
-        }
-      }
-    )
+
+  /**
+   * 获取专业 类目
+   * level => 1:返回全部层级  0:一层
+   * */
+  categoryMajor(data = {}, rnInfo) {
+    return post({ url: base_api + 'category/class', params: { parent_id: 0, level: 1, ...data }, rnInfo: rnInfo })
   },
-  cancelCollectArticle(id, rnInfo) {
-    var headers = {}
-    if (rnInfo) {
-      headers = {
-        Authorization: rnInfo.token,
-        uid: rnInfo.uid,
-        devicetype: rnInfo.deviceType,
-        token: rnInfo.token,
-        deviceid: rnInfo.deviceId,
-        version: rnInfo.version,
-        mobiletype: rnInfo.mobiletype
-      }
-    }
-    return instance.post(
-      base_api_883 + 'collection/delete',
-      {
-        sign: 'last',
-        timestamp: Number(new Date()),
-        type: 7,
-        cid: id
-      },
-      {
-        headers: {
-          appkey: 'compassedu',
-          ...headers
-        }
-      }
-    )
+
+  /**
+   * 获取语言 类目
+   * level => 1:返回全部层级  0:一层
+   * */
+  categoryLang(data = {}, rnInfo) {
+    return post({ url: base_api + 'category/class', params: { parent_id: 5, level: 1, ...data }, rnInfo: rnInfo })
   }
 }
--- src/base/loading/loading-end.vue
+++ src/base/loading/loading-end.vue
@@ -5,6 +5,7 @@
     </div>
     <div class="loading" v-if="loading">
       <img src="@/assets/img/loading.gif" />
+      <span v-if="text" class="loading-text">正在加载中</span>
     </div>
   </div>
 </template>
@@ -13,34 +14,39 @@
 export default {
   props: {
     end: false,
-    loading: false
+    loading: false,
+    text: ''
   }
 }
 </script>
 <style lang="less" scoped>
-@subcontent-font: 0.26rem;
-@sub-color: #80848f;
+@import url('~@/style/variables.less');
+@import url('~@/style/mixin.less');
 .wrapper {
   width: 100%;
   .end {
     width: 100%;
     height: 0.8rem;
     line-height: 0.8rem;
-    // background-color: #f7f7f7;
-    background-color: transparent;
+    background-color: #fff;
     font-size: @subcontent-font;
     color: @sub-color;
     text-align: center;
   }
   .loading {
     width: 100%;
-    height: 0.6rem;
+    height: 0.7rem;
     display: flex;
     justify-content: center;
     align-items: center;
     img {
       height: 0.4rem;
       width: 0.4rem;
+      margin-right: 10px;
+    }
+    .loading-text {
+      color: #8c8c8c;
+      font-size: 12px;
     }
   }
 }
